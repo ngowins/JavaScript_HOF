52ef15285824838226587a2061539a3e23b93a33
Nate Gowins <ngowins@gmail.com>
2022-03-29 11:02:21 -0500

    Completed HOF exercises

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plusNum(startValue) {
+  return function (n) {
+    return startValue + n;
+  };
+}
+
+const plusNum100 = plusNum(100);
+const plusNum15 = plusNum(15);
+
+console.log(plusNum15(15)); // 101
+console.log(plusNum100(100)); // 151
